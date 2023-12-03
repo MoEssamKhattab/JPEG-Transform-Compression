341c9318a270cd67bd1947340741a743a3967f47
MoEssamKhattab <s-mo.essam@zewailcity.edu.eg>
2023-12-03 08:50:53 +0200

    Another implementation of the 2d block into 1d array (updated)

--- zigzag_transform.py
+++ zigzag_transform.py
@@ -2,7 +2,14 @@
 
 import numpy as np
 
-def zigzag_transform(block, N):
+def zigzag_transform(block):
+    """
+    Return 1d array zigzag-traversed from the 2d block (N*N)
+    :param block: 2d block (N*N)
+    :return: 1d array (N*N)
+    """
+    N = block.shape[0]
+
     zigzag_indices = [(i,j) for i in range(N) for j in range(N)]
     zigzag_indices.sort(key = lambda x: (x[0], x[0]+x[1]) if (x[0]+x[1])%2 == 0 else (x[0]+x[1], x[1]))
 
