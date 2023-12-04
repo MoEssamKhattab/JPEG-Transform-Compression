8771d4f3cfdc0001e2a7d994403dd6409b78f074
Ahmed Mohamed <62614156+ahmedsamy972@users.noreply.github.com>
2023-12-05 01:04:18 +0200

    push last before final isa

--- decoder.py
+++ decoder.py
@@ -10,23 +10,21 @@ from Blockify.deblockify_image import deblockify_image
 def decoder(encoded_image,N,CompressionMode, HoriziontalPadding, VerticalPadding, HuffmanTree, no_vertical_blocks,no_horizontal_blocks):
 
     # # [1] apply Entropy decoding to encoded image
-    # entropy_decoded_image = np.array(huffman_decode(encoded_image,HuffmanTree))
+    entropy_decoded_image = np.array(huffman_decode(encoded_image,HuffmanTree))
     
     # # [2] apply run-length decoding
-    # runlength_decoded_image = run_length_decoder(entropy_decoded_image, no_vertical_blocks, no_horizontal_blocks, N)
+    runlength_decoded_image = run_length_decoder(encoded_image, no_vertical_blocks, no_horizontal_blocks, N)
     
     # [3] apply reverse zigzag transform (1D to 2D)
-    
-    runlength_decoded_image = encoded_image
-
+    zigzag_transformed_image = np.zeros((no_vertical_blocks,no_horizontal_blocks,N,N))
     blocks = np.zeros((no_vertical_blocks, no_horizontal_blocks ,N,N))
-    
+
     # image_total_pixels = no_vertical_blocks*no_horizontal_blocks*N*N
 
     index = 0
     for i in range(no_vertical_blocks):
         for j in range(no_horizontal_blocks):
-            block = runlength_decoded_image[index:index+N*N]
+            block = reverse_zigzag_transform(runlength_decoded_image[index:index+N*N],N)
             blocks[i][j] = block.reshape((N, N))
             index += N*N
 
--- encoder.py
+++ encoder.py
@@ -44,15 +44,14 @@ def encoder(image_array, N, CompressionMode):
                 idx += 1
     
     # [5] apply run-length encoding to each block
-    # run_length_encoded_blocks = np.array([])
+    run_length_encoded_blocks = np.array([])
 
-    # for i in range(total_no_blocks):
-    #         _1D_block = run_length_encoder(_1D_blocks[i])
-    #         run_length_encoded_blocks = np.append(run_length_encoded_blocks, _1D_block)
+    for i in range(total_no_blocks):
+         _1D_block = run_length_encoder(_1D_blocks[i])
+         run_length_encoded_blocks = np.append(run_length_encoded_blocks, _1D_block)
 
     # # [6] apply Entropy encoding to each block
-    # encoded_data, huffman_tree = huffman_encode(run_length_encoded_blocks)
+    encoded_data, huffman_tree = huffman_encode(run_length_encoded_blocks)
     
     # return encoded_data, huffman_tree, no_vertical_blocks, no_horizontal_blocks
-    
-    return _1D_blocks, None, no_vertical_blocks, no_horizontal_blocks
+    return run_length_encoded_blocks, huffman_tree, no_vertical_blocks, no_horizontal_blocks
